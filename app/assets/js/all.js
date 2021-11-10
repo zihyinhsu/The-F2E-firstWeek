f90f3ab317d08c771a7b034c58d573ff93f03ce1
ZY Hsu <jenny010328@gmail.com>
2021-11-10 10:39:20 +0800

    'firstWeek首頁-上半-ZY'

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,28 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+const swiper = new Swiper('.siteSwiper', {
+
+  slidesPerView: 1.3,
+  freeMode: true,
+  watchSlidesProgress: true,
+
+  spaceBetween: 20,
+   autoplay: {
+    // delay: 2500,
+    stopOnLastSlide: false,
+    disableOnInteraction: true,
+    },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2.8,
+      spaceBetween: 20
+    },
+    992: {
+      slidesPerView: 2.5,
+      spaceBetween: 20
+    }
+    
+  }
+})
