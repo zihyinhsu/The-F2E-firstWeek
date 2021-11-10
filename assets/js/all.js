8944bfde799fe64baa9dce1e906ebe4b5ba5dd0d
ZY Hsu <jenny010328@gmail.com>
2021-11-10 10:40:21 +0800

    Update 2021-11-10T02:40:10.945Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,27 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper('.siteSwiper', {
+  slidesPerView: 1.3,
+  freeMode: true,
+  watchSlidesProgress: true,
+  spaceBetween: 20,
+  autoplay: {
+    // delay: 2500,
+    stopOnLastSlide: false,
+    disableOnInteraction: true
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2.8,
+      spaceBetween: 20
+    },
+    992: {
+      slidesPerView: 2.5,
+      spaceBetween: 20
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
